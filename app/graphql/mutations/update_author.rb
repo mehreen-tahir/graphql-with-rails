f140374fbe297d1bdb00b451206a62b0ed121e6e
Mehreen Tahir <mehreen.tahir@marleyspoon.com>
2021-04-10 02:44:48 +0200

    Mutation: Update Author mutation created

--- app/graphql/mutations/update_author.rb
+++ app/graphql/mutations/update_author.rb
@@ -1,15 +1,17 @@
-class Mutations::CreateAuthor < Mutations::BaseMutation
-  description "Creates Author"
+class Mutations::UpdateAuthor < Mutations::BaseMutation
+  description "Updates Author"
 
   argument :name, String, required: true
-  argument :email, String, required: true
+  argument :id, ID, required: true
 
   field :author, Types::AuthorType, null: false
   field :errors, [String], null: false
 
-  def resolve(name:, email:)
-    author = Author.new(name: name, email: email)
-    if(author.save)
+  def resolve(name:, id:)
+    author = Author.find(id)
+    author.update_attributes(name: name)
+
+    if(author.errors.blank?)
       {author: author, errors: []}
     else
       {author: [], errors: author.errors.full_messages}
--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,5 +1,6 @@
 module Types
   class MutationType < Types::BaseObject
     field :create_author, mutation: Mutations::CreateAuthor
+    field :update_author, mutation: Mutations::UpdateAuthor
   end
 end
