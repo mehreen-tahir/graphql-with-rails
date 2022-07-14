db57eef7f1464841c37d5b831eab1f74e8342f22
shashankjo <kumarshashank.jo@gmail.com>
2022-07-14 23:25:42 +0530

    Add provision to update author email as well

--- app/graphql/mutations/update_author.rb
+++ app/graphql/mutations/update_author.rb
@@ -2,14 +2,15 @@ class Mutations::UpdateAuthor < Mutations::BaseMutation
   description "Updates Author"
 
   argument :name, String, required: true
+  argument :email, String, required: true
   argument :id, ID, required: true
 
   field :author, Types::AuthorType, null: false
   field :errors, [String], null: false
 
-  def resolve(name:, id:)
+  def resolve(name:, id:, email:)
     author = Author.find(id)
-    author.update_attributes(name: name)
+    author.update(name: name, email: email)
 
     if(author.errors.blank?)
       {author: author, errors: []}
